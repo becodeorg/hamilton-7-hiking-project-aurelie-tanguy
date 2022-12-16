4966edeff93d2e185d7718113f02e21abc3626fc
TanguyC0 <corman.tanguy.ct@gmail.com>
2022-12-16 13:55:00 +0100

    add methode for add and update the user

--- src/models/Users.php
+++ src/models/Users.php
@@ -17,4 +17,47 @@ class Users extends Database
             return [];
         }
     }
+
+    public function add(string $firstname, string $lastname, string $nickname, string $email, string $password): bool
+    {
+        try
+        {
+            $stmt = $this->query('INSERT INTO users (firstname, lastname, nickname, email, password) 
+            VALUES (:firstname, :lastname, :nickname, :email, :password)', [
+                'firstname' => $firstname,
+                'lastname' => $lastname,
+                'nickname' => $nickname,
+                'email' => $email,
+                'password' => $password
+            ]);
+            return true;
+        }
+        catch (PDOException $e)
+        {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function update(int $id, string $firstname, string $lastname, string $nickname, string $email, string $password): bool
+    {
+        try
+        {
+            $stmt = $this->query('UPDATE users SET firstname = :firstname, lastname = :lastname, nickname = :nickname,
+                email = :email, password = :password WHERE id = :id', [
+                    'id' => $id,
+                    'firstname' => $firstname,
+                    'lastname' => $lastname,
+                    'nickname' => $nickname,
+                    'email' => $email,
+                    'password' => $password
+            ]);
+            return true;
+        }
+        catch (PDOException $e)
+        {
+            echo $e->getMessage();
+            return false;
+        }
+    }
 }
