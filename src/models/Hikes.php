a05762155c67a821673a6be63b40fbcce80fca6b
TanguyC0 <corman.tanguy.ct@gmail.com>
2022-12-16 13:44:38 +0100

    add method for find by tag

--- src/models/Hikes.php
+++ src/models/Hikes.php
@@ -22,8 +22,22 @@ declare(strict_types=1);
         {
             try
             {
-                $stmt = $this->query('SELECT * FROM tmp WHERE id = :id', ['id' => $id]);
+                $stmt = $this->query('SELECT * FROM hikes join users on ( hikes.createur = users.id) WHERE hikes.id = :id', ['id' => $id]);
                 return $stmt->fetch();
+            }
+            catch (PDOException $e)
+            {
+                echo $e->getMessage();
+                return [];
+            }
+        }
+
+        public function findTags(int $id): array|false
+        {
+            try
+            {
+                $stmt = $this->query('SELECT * FROM tags join hikes_tags on ( tags.id = hikes_tags.tag_id) WHERE hikes_tags.hike_id = :id', ['id' => $id]);
+                return $stmt->fetchAll();
             }
             catch (PDOException $e)
             {
