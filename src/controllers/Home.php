94eee6994a21a4ed1bc6f538caf211ee698ca2cc
TanguyC0 <corman.tanguy.ct@gmail.com>
2022-12-19 09:38:34 +0100

    fix random

--- src/controllers/Home.php
+++ src/controllers/Home.php
@@ -7,6 +7,7 @@ class Home implements Icontrollers
     public function startcontroller($function) :void
     {
         $this->$function();
+        include 'views/home.php';
         include 'views/template.php';
     }
 
--- src/core/Routeur.php
+++ src/core/Routeur.php
@@ -7,9 +7,9 @@ class Router
     public function root() : void
     {
         $request = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'); // récupére la page/chemin dans l'url
-        [$request,$function] = explode('/', $request);
+        [$request,$function] = explode('/', $request); // class/methode
 
-        method_exists($request, 'startcontroller') ? $method = $_SERVER['REQUEST_METHOD'] : $method = 'GET'; // récupére la méthode dans l'url (GET, POST, PUT, DELETE) ici seul get et post sont utilisé
+       // method_exists($request, 'startcontroller') ? $method = $_SERVER['REQUEST_METHOD'] : $method = ''; // récupére la méthode dans l'url (GET, POST, PUT, DELETE) ici seul get et post sont utilisé
 
         //$method = $_SERVER['REQUEST_METHOD']; // récupére la méthode dans l'url (GET, POST, PUT, DELETE) ici seul get et post sont utilisé
 
