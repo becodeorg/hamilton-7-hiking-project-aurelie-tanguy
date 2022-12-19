aae6170dfdeec098e222a2390bc037c72a73743f
Elhya13 <aureliewinkin@gmail.com>
2022-12-19 12:19:07 +0100

    hikes add and remove

--- src/models/Hikes.php
+++ src/models/Hikes.php
@@ -45,4 +45,60 @@ declare(strict_types=1);
                 return [];
             }
         }
+
+        public function add(string $name, int $id, float $distance, string $duration, float $elevation_gain, string $description): bool
+        {
+            try
+            {
+                $stmt = $this->query('INSERT INTO hikes (name, id, distance, duration, elevation_gain, descirption) 
+                VALUES (:name, :id, :datecreation, :distance, :elevation_gain, :description )', [
+                    'id' => $id,
+                    'name' => $name,
+                    'distance' => $distance,
+                    'duration' => $duration,
+                    'elevation_gain' => $elevation_gain,
+                    'description' => $description
+                ]);
+                return true;
+            }
+            catch (PDOException $e)
+            {
+                echo $e->getMessage();
+                return false;
+            }
+        }
+        public function update(int $id, string $name, float $distance, string $duration, float $elevation_gain, string $description): bool
+        {
+            try
+            {
+                $stmt = $this->query('UPDATE hikes SET name = :name, id = :id WHERE id = :id', [
+                        'id' => $id,
+                        'name' => $name,
+                        'distance' => $distance,
+                        'duration' => $duration,
+                        'elevation_gain' => $elevation_gain,
+                        'description' => $description
+                ]);
+                return true;
+            }
+            catch (PDOException $e)
+            {
+                echo $e->getMessage();
+                return false;
+            }
+        }
+        public function delete(int $id): bool
+        {
+            try
+            {
+                $stmt = $this->query('DELETE FROM hikes WHERE id = :id', ['id' => $id]);
+                return true;
+            }
+            catch (PDOException $e)
+            {
+                echo $e->getMessage();
+                return false;
+            }
+        }
+    
     }
