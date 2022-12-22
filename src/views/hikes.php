aa99f635911a65cdde36431fbc5991533af67c57
Elhya13 <aureliewinkin@gmail.com>
2022-12-22 13:42:09 +0100

    try tag function

--- src/views/hikes.php
+++ src/views/hikes.php
@@ -3,10 +3,16 @@
 
     ?>
 
-    <form action="">
-        <p>ici pour trier</p>
+    <form action="Hikes/list" method="GET">
+        <label for="tag">Search by Tag:</label>
+        <input type="search" id="search_tag" name="search_tag"><br>
+        <button> Search </button>
     </form>
 
     <?php
+    if (isset ($_GET["search_tag"])) {
+        function findTags
+   }
+
     echo $content;
     $content = ob_get_clean();
