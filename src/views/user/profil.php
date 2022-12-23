e12e4b8b648d250469285eacb2b47b31718931d6
TanguyC0 <corman.tanguy.ct@gmail.com>
2022-12-23 13:08:30 +0100

    add display data in profil user

--- src/views/user/profil.php
+++ src/views/user/profil.php
@@ -16,7 +16,7 @@
                 <div>
                     <p class="text-indigo-300 text-sm font-medium uppercase leading-4">FIRSTNAME</p>
                     <p class="text-white font-bold text-2xl inline-flex items-center space-x-2">
-                        <span>name</span>
+                        <span><?= $_SESSION["user"]["firstname"]?></span>
                     </p>
                 </div>
             </div>
@@ -31,7 +31,7 @@
                 <div>
                     <p class="text-indigo-300 text-sm font-medium uppercase leading-4">LASTNAME</p>
                     <p class="text-white font-bold text-2xl inline-flex items-center space-x-2">
-                        <span>name</span>
+                        <span><?= $_SESSION["user"]["lastname"]?></span>
                     </p>
                 </div>
             </div>
@@ -46,7 +46,7 @@
                 <div>
                     <p class="text-indigo-300 text-sm font-medium uppercase leading-4">NICKNAME</p>
                     <p class="text-white font-bold text-2xl inline-flex items-center space-x-2">
-                        <span>name</span>
+                        <span><?= $_SESSION["user"]["nickname"]?></span>
                     </p>
                 </div>
             </div>
@@ -61,7 +61,22 @@
                 <div>
                     <p class="text-indigo-300 text-sm font-medium uppercase leading-4">EMAIL</p>
                     <p class="text-white font-bold text-2xl inline-flex items-center space-x-2">
-                        <span>name</span>
+                        <span><?= $_SESSION["user"]["email"]?></span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="bg-black/60 to-white/5 p-6 rounded-lg">
+            <div class="flex flex-row space-x-4 items-center">
+                <div id="stats-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 text-white">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                        </svg>
+                </div>
+                <div>
+                    <p class="text-indigo-300 text-sm font-medium uppercase leading-4">create</p>
+                    <p class="text-white font-bold text-2xl inline-flex items-center space-x-2">
+                        <span><?= $_SESSION["user"]["created_at"]?></span>
                     </p>
                 </div>
             </div>
